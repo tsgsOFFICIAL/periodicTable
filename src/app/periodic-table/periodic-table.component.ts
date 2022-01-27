dbeaf14022fab7a9608a512c849f5befdd6553e0
Marcus Jensen <marcusproman0507@gmail.com>
2022-01-27 19:33:09 +0100

    Successfully loaded all 118 elements from API

--- src/app/periodic-table/periodic-table.component.ts
+++ src/app/periodic-table/periodic-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { PeriodicTableElement } from '../interface/periodic-table-element';
 
 @Component({
   selector: 'app-periodic-table',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeriodicTableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
+  elements: PeriodicTableElement[] = [];
+  
   ngOnInit(): void {
+    this.apiService.get().subscribe((elements: PeriodicTableElement[]) => {
+      next: this.elements = elements;
+      complete: if (this.elements.length >= 1) {
+        // console.log(`We're ready for take-off!`);
+        // console.log(this.elements[0]);
+        // console.log(elements[0]);
+      }
+    });
   }
 
 }
